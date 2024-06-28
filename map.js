c6a6c4f029eb6e0c0d15f1de4ad6cd3b7211108c
Mathew M <mathewmastromonaco@gmail.com>
2024-06-28 03:42:30 -0400

    save.

---
+++ map.js
@@ -0,0 +1,20 @@
+// Our map function will take in two arguments:
+// 
+//     - An array to map
+//     - A callback function
+// 
+// The map function will return a new array based on the results of the callback function.
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word [0]);
+console.log(results1);
