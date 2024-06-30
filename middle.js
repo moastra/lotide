84e5e8c10835c1212fa059950006aa502c690f8a
Mathew M <mathewmastromonaco@gmail.com>
2024-06-30 18:50:06 -0400

    Created middle function.

---
+++ middle.js
@@ -0,0 +1,43 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const middle = function(arr) {
+  const length = arr.length;
+  const mid = Math.floor(length/2);
+
+  if (length <= 2) {
+    return [];
+  }
+  if (length % 2 === 0) {
+    return [arr[mid-1], arr[mid]];
+  } else {
+    return [arr[mid]];
+  }
+};
+
+
+// TEST CODE
+assertEqual(eqArrays(middle([1]), []), true);
+assertEqual(eqArrays(middle([1, 2]), []), true);
+assertEqual(eqArrays(middle([1, 2, 3]), [2]), true);
+assertEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true);
+assertEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
+assertEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true);
