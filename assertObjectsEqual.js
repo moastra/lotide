cf2907351c5b4788315559ea46bdf8717fc91367
Mathew M <mathewmastromonaco@gmail.com>
2024-06-30 23:08:05 -0400

    Fixed the inspect function by logging it

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -36,13 +36,12 @@ const eqObjects = function(object1, object2) {
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect;
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect.actual} === ${inspect.expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect.actual} !== ${inspect.expected}`);
   }
 };
 const numbers1 = { a: '1', b: 2 };
 const numbers2 = { b: 2, a: '1' };
 
 assertObjectsEqual(numbers1, numbers2);
-console.log(`Example label: ${inspect(actual)}`);
