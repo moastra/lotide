e3e0d6a3e825be8bcdebb409f838dc473b44340f
Mathew M <mathewmastromonaco@gmail.com>
2024-07-01 00:31:11 -0400

    Created the findKey function.

---
+++ findKey.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const restaurants = {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
+const results1 = findKey(restaurants, x => x.stars === 2);
+console.log(results1);
+assertEqual(results1, 'noma');
+
+const results2 = findKey(restaurants, x => x.stars === 3);
+console.log(results2);
+assertEqual(results2, 'Akaleri');
