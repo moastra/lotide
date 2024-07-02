2a4df05e67413fe606bb02533de88dc7a31d9815
Mathew M <mathewmastromonaco@gmail.com>
2024-07-02 10:38:09 -0400

    Cleaned up the tail.js file.

--- tail.js
+++ tail.js
@@ -4,9 +4,4 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-const eqArrays = require("./eqArrays");
-
-const assertArraysEqual = require("./assertArrayEqual");
-
-
 module.exports = tail;
